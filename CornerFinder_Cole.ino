cd8b6cd5f1c1d3cccbddf2b6f1d45b6ebcbcdc0e
Cole <cole.smith@xtra.co.nz>
2024-03-14 00:21:36 +1300

    Corner Finder Functionality Drafted

--- CornerFinder_Cole.ino
+++ CornerFinder_Cole.ino
@@ -1,94 +1,120 @@
-void rotate90CW_OL();
+void stop();
 
 
 // Corner Finder
-void cornerFindOpenLoop(float sonarDistance){
-  float longest = 0;
-  float time_elsapsed = 0;
+void findCorner(float sonarDistance, float shortIR){
+  static int runState = 0;
+  static int prevRunState = 0;
+  static float maxSonar = 0;
   
-  //spin robot 90 degrees => may need gyro for this?
-  //(don't need to do full 360 as at least one corner will be passed in 90 degrees)
+  int time45deg = 2000;
 
-  //Could also rotate for a set amount of time that we know will pass 90 degrees reliably
+  switch (runState){
+    //Reverse to wall
+    case 0:
+      static int numStationary = 0;
+      static int  prevSonar = 0;
+      reverse();
 
-  //poll this while rotating
-  if(sonarDistance > longest){
-    longest = sonarDistance;
-  }
+      //keep count of how often sonar reading doesn't change
+      if ((sonarDistance - prevSonar) < threshold)
+        numStationary++;
 
-  while(sonarDistance < longest){
-    //turn robot a little bit
-    ccw();
-  }
-
-  while(sonarDistance > 20){
-    forward();
-  }
-
-  averageShort = (distanceSF + distanceSB)/2;
-  averageLong = (distanceLF + distanceLB)/2;
-
-
-
-  if(averageShort <= 30){ 
-
-    while(averageShort > 10){
-      // move robot right till wall
-    }
-  }
-  else if(averageLong <= 80){
-    if(averageLong < 60){
-      while(averageLong > 10){
-        //move left till wall
+      prevSonar = sonarDistance;
+      
+      //if sonar detects stationary enough times, move to next state
+      if(numStationary > 5){
+        stop();
+        prevRunState = runState;
       }
-    }
-    else {
-      while(averageLong > 10){
-        // move robot right till wall
+
+      if(prevRunState == 4)
+        runState = 5;
+      else
+        runState++;
+
+      break;
+
+    //Detect which wall
+    case 1:
+      if (sonarDistance > 135)
+        runState++;
+      else
+        runState+=2;
+      
+      break;
+
+    //Short walls
+    case 2:
+      strafe_left();
+
+      if (shortIR < 15){
+        stop();
+        prevRunState = runState;
+        runState = 6;
       }
-    }
-  }
+      break;
+
+    //Long walls pt. 1 (move forwards)
+    case 3:
+      static int sonarInit = sonarDistance;
+      
+      forward();
+
+      if ((sonarInit - sonarDistance) > 5){
+        stop();
+        prevRunState = runState;
+        runState++;
+      }
+      break;
+
+    //Long walls pt. 2 (rotate CCW)
+    case 4:
+      static int timeElapsed = 0;
+
+      if (timeElapsed >= time45deg){
+        //P controller to get 0 angle => exit to case 0
+      } else
+        ccw();
+
+      timeElapsed += T;
+      break;
+
+    //controller finished
+    case 6:
+      break;
+  };
   
-  Serial.println("Am I in a corner?");
-
-///////////////////////////////////
-
-  /*float shortLF = 0;
-  float shortLB = 0;
-  bool turnRight = 0;
-  
-  // spin robot 360 degrees
-  
-  if(distanceLF > shortLF){shortLF = distanceLF;}
-  if(distanceLB > shortLB){shortLB = distanceLFB;}
-   float aveShort = (distanceLF + distanceLB)/2;
-
-  // spin robot again till you refind the aveShort distance
-
-  if(sonarDistance > 100){
-    turnRight = 1;
-    shortSonar = 200 - sonarDistance;
-    while(sonarDistance > shortSonar){ 
-      // turn robot a little bit more than 90 degrees 
-      // turn robot small amounts after that
-    }
-    
-  }
-
-  // move forward till 10cm or something.... using sonar sensor
-  if(turnRight){ // have to turn right as the short side has the long distance sensors vs short, could move right first using long distance then turn to sonar later aswell
-
-    //turn right 90 degrees 
-    // move forward till 10 cm...
-  }
-  else{
-    // move left till short sensors pick up 10 cm 
-  }
- */
-
-
- 
 }
 
 
 
+//redundant code to find the corner directly ahead
+/*
+    //Scan for corner distance
+    case 0:
+      static int timeElapsed = 0;
+
+      if (timeElapsed >= time90deg){
+        stop();
+        runState++;
+        break;
+      }
+
+      cw();
+
+      if (maxSonar < sonarDistance)
+        maxSonar = sonarDistance;
+
+      timeElapsed += T;
+      break;
+    
+    //Align with corner
+    case 1:
+      if (sonarDistance >= (maxSonar - sonarTolerance)) {
+        stop();
+        runState++;
+        break;
+      }
+      break;
+*/
